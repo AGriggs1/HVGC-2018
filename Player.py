03905a00193a6c64548c8858d9018fb3844e5e2c
Anthony Griggs <Anthony.Griggs1@marist.edu>
2018-10-25 14:24:39 -0400

    Player Class

---
+++ Player.py
@@ -0,0 +1,13 @@
+#Anthony Griggs
+#HVGC 2018 Game
+#Player class
+
+class Player:
+    #Define the constructor
+    def __init__(self):
+        self.Name = "Pan"
+        self.Moves = 0
+        self.Score = 0
+        self.iLocale = 0
+        self.Inventory = []
+
