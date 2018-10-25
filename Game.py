2a8ad9c4aaeeb22d60266cd15eb49849d007a2f7
Anthony Griggs <Anthony.Griggs1@marist.edu>
2018-10-25 19:17:53 -0400

    Began adding Locales. Next to come is the Navigation Matrix before I continue with the rest.

--- Game.py
+++ Game.py
@@ -6,6 +6,15 @@ from Player import *
 from Locale import *
 
 gCont = "continue"
+
+
+#Define Locales
+pParkingE = Locale(0, "You finally enter a parking space and get out of your car. You packed lightly, so you don't think you'll be needing the help of bellhop.", "You are in the parking lot", "There doesn't seem many cars here. In fact, the only person around is the man that's talking to you right now. That seems like a good thing, right?", [])
+pParkingW = Locale(1, "You begin to make your way towards the hotel entrance.", "You are in the parking lot", "Sunrise Hotel. It gives a nice tropical vibe, doesn't it? Too bad there's no beach nearby!", [])
+pEntrance = Locale(2, "You reach the entrance to the hotel. You are eager to check in and officially begin your time off.", "You are at the entrance of the hotel.", "", [])
+pLobby = Locale(3, "You enter the hotel lobby. Quaint.", "You are in the hotel lobby.", "Like it's name, the lobby of the hotel is themed with a tropical mindset. It's a bit on the cheesy side, actually.", [])
+pFrDesk = Locale(4, "You make your way towards the front desk, the base of which is painted with light-blue wave crests.", "You are at the front desk", "", ["Pen"])
+pCafe = Locale(5, "You head into the hotel cafe. The smell of coffee overwhelms you.", "You are in the cafe", "The cafe has a small-town coffee shop vibe. It's a nice change from the tropical one, that's for sure!", [])
 ####################
 ##UTILITY FUNCTIONS
 ####################
@@ -46,7 +55,8 @@ def Init():
     Pan = Player()
     print("\nIt's that time of year again. The sun's rays rain down on you as you as pull into the parking lot, the air endowed with the summer scent. Vacation time.\n")
     prompt(gCont)
-    print("You decided to keep things simple this year for once. Nothing too extravagent. Just an out-of town trip at a humble hotel, the Sunrise Inn. Ah.\n")
+    print("You decided to keep things simple this year for once. Nothing too extravagent. Just an out-of town trip at the nice, but humble, the Sunrise Hotel. Ah.\n")
     prompt(gCont)
+    print(pParkingE.LongDesc)
 Init()
 
