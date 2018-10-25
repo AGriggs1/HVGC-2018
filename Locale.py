4bf540ffc06a207647d501852bd85839eafcba53
Anthony Griggs <Anthony.Griggs1@marist.edu>
2018-10-25 14:32:04 -0400

    Locale class

---
+++ Locale.py
@@ -0,0 +1,19 @@
+#Anthony Griggs
+#HVGC 2018 Game
+#Locale class
+
+class Locale:
+    #Define the constructor
+    def __init__(self, ID, sLongDesc, sShortDesc, sExamineDesc, tItems):
+        self.ID = ID
+        self.LongDesc = sLongDesc
+        self.ShortDesc = sShortDesc
+        #We need to create a new table for the items
+        #Doing self.Items = tItems creats a reference
+        self.Items = []
+        for sItem in tItems: Items.append(sItem)
+    #End Constructor
+
+    #TODO: AddItem, RemoveItem
+
+       
