dfeef01a42c93eb1b39b8e0819f9d4146c37c010
Anthony Griggs <Anthony.Griggs1@marist.edu>
2018-10-26 08:26:21 -0400

    Added a table where the indices hold a location with the same ID. Tested navigation, seems to be working fine.

--- Game.py
+++ Game.py
@@ -6,7 +6,7 @@ from Player import *
 from Locale import *
 
 gCont = "continue"
-
+Pan = Player()
 
 #Define Locales
 pParkingE = Locale(0, "You finally enter a parking space and get out of your car. You packed lightly, so you don't think you'll be needing the help of bellhop.", "You are in the parking lot", "There doesn't seem many cars here. In fact, the only person around is the man that's talking to you right now. That seems like a good thing, right?", [])
@@ -16,6 +16,15 @@ pLobby = Locale(3, "You enter the hotel lobby. Quaint.", "You are in the hotel l
 pFrDesk = Locale(4, "You make your way towards the front desk, the base of which is painted with light-blue wave crests.", "You are at the front desk", "", ["Pen"])
 pCafe = Locale(5, "You head into the hotel cafe. The smell of coffee overwhelms you.", "You are in the cafe", "The cafe has a small-town coffee shop vibe. It's a nice change from the tropical one, that's for sure!", [])
 
+#Define the Locations table
+#This is used to hook Players, NPCs to the Locale objects
+#We can't create a reference to the location from Player, NPC, as Locations have may potentially contain data that is subject to change throughout the game (items, visitation)9
+tLocations = [pParkingE,
+              pParkingW,
+              pEntrance,
+              pLobby,
+              pFrDesk,
+              pCafe]
 
 #Define the Navigation Matrix
 mNavigator = [
@@ -55,14 +64,25 @@ def prompt(sMessage):
     input("Print enter to " + sMessage)
 #End prompt
 
-        
+#####################
+##move
+##Attempts to move the player in passed direction
+#####################
+def move(iDirection):
+    pLocation = mNavigator[Pan.iLocale][iDirection]
+    if  pLocation:
+        #The player can go here. Do so.
+        Pan.iLocale = pLocation.ID
+        Pan.Moves += 1
+        tLocations[pLocation.ID].UpdateVisited()
+    else: print("You cannot go that way.")
+#End move
 
 ##########
 ##Init
 ##Initialization function. Runs at game start
 ##########
 #Initialize the player
-Pan = Player()
 def Init():
     copyright(False)
     
@@ -70,7 +90,11 @@ def Init():
     prompt(gCont)
     print("You decided to keep things simple this year for once. Nothing too extravagent. Just an out-of town trip at the nice, but humble, the Sunrise Hotel. Ah.\n")
     prompt(gCont)
-    print(pParkingE.LongDesc)
+    print(tLocations[Pan.iLocale].LongDesc)
+    move(0) #0 = North
+    move(3) #3 = West
+    prompt(gCont)
+    print(tLocations[Pan.iLocale].LongDesc)
 
 def Game():
     bGame = True;
