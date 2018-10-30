5e29183370897820ebad6b9cb9c0b46b873d3d31
Anthony Griggs <Anthony.Griggs1@marist.edu>
2018-10-30 17:22:31 -0400

    Set up ground work for interactives.

--- Game.py
+++ Game.py
@@ -10,23 +10,30 @@ gCont = "continue"
 Pan = Player()
 
 #Define Locales
-pParkingE = Locale(0, "You finally enter a parking space and get out of your car. You packed lightly, so you don't think you'll be needing the help of bellhop.", "You are in the parking lot", "There doesn't seem many cars here. In fact, the only person around is the man that's talking to you right now. That seems like a good thing, right?", [])
-pParkingW = Locale(1, "You begin to make your way towards the hotel entrance.", "You are in the parking lot", "Sunrise Hotel. It gives a nice tropical vibe, doesn't it? Too bad there's no beach nearby!", [])
+pParkingE = Locale(0, "You finally enter a parking space and get out of your car. You packed lightly, so you don't think you'll be needing the help of bellhop.", "You are in the parking lot",
+                   "There doesn't seem many cars here. In fact, the only person around is the man that's talking to you right now. That seems like a good thing, right?",
+                   ["Your car"])
+pParkingW = Locale(1, "You begin to make your way towards the hotel entrance.", "You are in the parking lot", "Sunrise Hotel. It gives a nice tropical vibe, doesn't it? Too bad there's no beach nearby!",
+                   [])
 pEntrance = Locale(2, "You reach the entrance to the hotel. You are eager to check in and officially begin your time off.", "You are at the entrance of the hotel.", "", [])
-pLobby = Locale(3, "You enter the hotel lobby. Quaint.", "You are in the hotel lobby.", "Like it's name, the lobby of the hotel is themed with a tropical mindset. It's a bit on the cheesy side, actually.", [])
-pFrDesk = Locale(4, "You make your way towards the front desk, the base of which is painted with light-blue wave crests.", "You are at the front desk", "", [])
-pCafe = Locale(5, "You head into the hotel cafe. The smell of coffee overwhelms you.", "You are in the cafe", "The cafe has a small-town coffee shop vibe. It's a nice change from the tropical one, that's for sure!", [])
-pHallway = Locale(6, "You head down a hallway.", "You are in a hallway.", "There are doors to rooms on each end. Further down the hallway is a door leading outside.", [])
-pRecRoom = Locale(7, "You enter what looks like to be rec room.", "You are in a rec room.", "Arcade machines line the wall. There is a pool table and lounge chairs litter the room. Cozy.", [])
-pElevator = Locale(8, "It's an elevator. You push the call button and step inside.", "It's an elevator.", "A Latin tune is leaking from the a speaker on the ceiling.", [])
-pPool = Locale(9, "You head outside and come to a pool.", "You are at the pool.", "Very fitting, for once.", [])
-pHotub = Locale(10, "You walk along the poolside and come to a hot tub.", "You are by the hot tub", "It looks very inviting, actually!", [])
-pCabana = Locale(11, "You head towards what looks like a shop themed to be a Beach Cabana.", "You are at the cabana.", "It appears to be made out of straw and bamboo. A familiar Latin tune is blasting from the radio.", [])
+pLobby = Locale(3, "You enter the hotel lobby. Quaint.", "You are in the hotel lobby.", "Like it's name, the lobby of the hotel is themed with a tropical mindset. It's a bit on the cheesy side, actually.",
+                ["Couch", "Magazines", "You see a Map of the Building."])
+pFrDesk = Locale(4, "You make your way towards the front desk, the base of which is painted with light-blue wave crests.", "You are at the front desk", "",
+                 ["Bell"])
+pCafe = Locale(5, "You head into the hotel cafe. The smell of coffee overwhelms you.", "You are in the cafe", "The cafe has a small-town coffee shop vibe. It's a nice change from the tropical one, that's for sure!",
+               ["Table"])
+pHallway = Locale(6, "You head down a hallway.", "You are in a hallway.", "There are doors to rooms on each end. Further down the hallway is a door leading outside.",
+                  [])
+pRecRoom = Locale(7, "You enter what looks like to be rec room.", "You are in a rec room.", "Arcade machines line the wall. There is a pool table and lounge chairs litter the room. Cozy.", ["Arcade Cabinent", "Pool Table", "Couch"])
+pElevator = Locale(8, "It's an elevator. You push the call button and step inside.", "It's an elevator.", "A Latin tune is leaking from the a speaker on the ceiling.", ["Elevator"])
+pPool = Locale(9, "You head outside and come to a pool.", "You are at the pool.", "Lounge Chairs litter the poolside. Very fitting, for once.", ["Pool, Lounge Chairs"])
+pHotub = Locale(10, "You walk along the poolside and come to a hot tub.", "You are by the hot tub", "It looks very inviting, actually!", ["Hot tub"])
+pCabana = Locale(11, "You head towards what looks like a shop themed to be a Beach Cabana.", "You are at the cabana.", "It appears to be made out of straw and bamboo. A familiar Latin tune is blasting from the radio.", ["Table"])
 pHallway2 = Locale(12, "You enter a hallway", "You are in a hallway",  "", [])
 pHallway3 = Locale(13, "You continue down the hallway", "You are in a hallway", "", [])
 pHallway4 = Locale(14, "You continue down the hallway", "You are in a hallway", "", [])
-pBedroom = Locale(15, "You open the door and enter your room.", "You are in your room.", "Not too bad. Two queen-sized beds, a nightstand, a bathroom, and a TV. You even have a balcony!", [])
-pBalcony = Locale(16, "You head out onto the balcony.", "You are at your room's balcony", "The view's pretty nice. You needed this, you tell yourself.", [])
+pBedroom = Locale(15, "You open the door and enter your room.", "You are in your room.", "Not too bad. Two queen-sized beds, a nightstand, a bathroom, and a TV. You even have a balcony!", ["Bed", "TV"])
+pBalcony = Locale(16, "You head out onto the balcony.", "You are at your room's balcony", "The view's pretty nice. You needed this, you tell yourself.", ["Chair"])
 #Define the Locations table
 #This is used to hook Players, NPCs to the Locale objects
 #We can't create a reference to the location from Player, NPC, as Locations have may potentially contain data that is subject to change throughout the game (items, visitation)
@@ -522,7 +529,11 @@ def onDialogueEnd(iNPC):
         if pNPC.Progress == 1:
             pNPC.Progress = 2
             Pan.Inventory.append("Cabana Slushie")
-    
+    ##end elif
+#end onDialogueEnd
+
+def use(sThing, iLocale):
+    pass
     
 ############
 ##reset
@@ -540,7 +551,8 @@ def reset():
     Pan.iLocale = 0
     Pan.Inventory = []
 #End reset
-    
+
+def Init
 ##########
 ##Init
 ##Initialization function. Runs at game start
@@ -573,7 +585,7 @@ sHelp = ("List of commands\n"
         "Moves: List the amount of moves you have made so far. A move is generally defined as changing from one location to another, but it is not limited to that.\n"
         "Talk to: Used to interact with any people who may be in the area.\n"
         "Use: Used to interact with things in the environment you are at.\n"
-        "Inventory: Lists any items you may be holding.\n")
+        "Inventory: Lists any items you are currently holding.\n")
 def Game():
     bGame = True;
     while(bGame):
@@ -601,7 +613,15 @@ def Game():
             #end if
         elif sInput[0:7] == "talk to" and sInput != "talk to" and tLocations[Pan.iLocale].Examined: #This means the player may have specified a specific player
             talkTo(sInput[7:len(sInput)].strip())
-        elif sInput == "use": pass #This one will take a lot to make sure it's done right. Depends on items used
+        elif sInput == "use":
+            print("Use?")
+            if tLocations[Pan.iLocale].Examined:
+                for s in tLocations[Pan.iLocale].Interactives:
+                    print(s)
+                    #Got the user input
+                #end for
+                sInput = input().lower
+                use(sInput, Pan.iLocale)
         elif sInput == "inventory":
             print("You have: ")
             for s in Pan.Inventory: print(s)
